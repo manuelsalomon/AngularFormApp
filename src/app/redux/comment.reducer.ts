2478fd678da86fc795eb74bf5fe7d0f4f399c1c0
Solano Palacio <solanoepalacio@gmail.com>
2017-02-17 12:44:14 -0300

    deleted the files outside the frontend folder

--- src/app/redux/comment.reducer.ts
+++
@@ -1,7 +0,0 @@
-import { Action, Reducer }  from 'redux';
-import * as AppActions from './actions';
-import { Comment } from '../models/comment.model';
-
-export interface CommentState{
-  comments: Comment[];
-}
--- src/app/redux/user.reducer.ts
+++
@@ -1,9 +0,0 @@
-import { Action, Reducer }  from 'redux';
-import * as AppActions from './actions';
-import { User } from '../models/user.model';
-
-export interface UserState {
-  isLogged: boolean,
-  username: string,
-  name: string,
-}
